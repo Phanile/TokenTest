0a2586a5b690800397117dc21024bbe20ebb4803
Phanile <pasha_vasilev_2003@mail.ru>
2020-12-26 21:17:03 +0300

    create Profile model

--- tokentest/tokencore/models.py
+++ tokentest/tokencore/models.py
@@ -1,3 +1,36 @@
+import random
+import string
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    objects = models.Manager()
+    user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE, related_name='user')
+
+    referral_code = models.CharField(max_length=50)
+    referred_by = models.OneToOneField(User, unique=True, on_delete=models.CASCADE, related_name='referred_by')
+
+    email_verified = models.BooleanField()
+    mobile_phone_verified = models.BooleanField()
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    mobile_phone = models.CharField(max_length=10)
+
+    created_at = models.DateField()
+    last_login = models.DateField()
+
+    def create_referral_code(self, size):
+        return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(size))
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
